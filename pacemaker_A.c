a6f8b4e4786a714c0544f13029984f6b48878d5a
jby5 <jby5@duke.edu>
2015-11-17 16:50:39 -0500

    cleaned

--- pacemaker_A.c
+++ pacemaker_A.c
@@ -1,8 +1,9 @@
 /* 
  * File:   pacemaker_A.c
- * Author: Jessica
+ * Author: Jessica Yan
  *
  * Created on November 1, 2015, 1:36 AM
+ * Last modified: 11/17/2015 4:50PM
 
  * 1. Detect atrial EGM by detecting slew rate + threshold
  * 2. if EGM detected, output 10 cycles of a 50kHz PWM signal 
@@ -10,10 +11,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-//#include <delays.h>
 #include <htc.h>
-//#include <pic16f1827.h>
-//#pragma config1 FOSC = FOSC_INTOSC //internal oscillator, I/O function on CLKIN pin 
 #define _XTAL_FREQ 0x3D0900//4MHz system clock
 
 void processA(void);
@@ -22,23 +20,24 @@ void SysInit(void);
 
 int detected; 
 int stateA;
+int slewThresh;
+int ampThresh;
 
 void main(void) {
+    stateA = 0;
     SysInit();
     
     while(1){
-        
+        stateA = 0;
         processA();
         
         if (stateA == 1){
             output();
             stateA = 0;
-        __delay_ms(200); //delay 200ms to "debounce"    
+        __delay_ms(300); //delay 300ms to "debounce"    
             
         }
-        
     }
-     
 }
 
 void SysInit(void){
@@ -54,8 +53,7 @@ void SysInit(void){
     ADCON0bits.ADON = 0x01; //enable ADC module 
     
     TRISBbits.TRISB0 = 0; 
-    
-    
+       
     //PWM
     T2CONbits.TMR2ON = 1; //1MHz timer
     CCP3CONbits.DC3B = 0b00;
@@ -65,40 +63,41 @@ void SysInit(void){
     CCPR3L = 0b00001010; //8 MSBs of period 
     
     TRISAbits.TRISA3 = 0; //CCP3 output
-    
 }
 
 void processA(void){ //ADC and calculation of slew rate/threshold
-    
-    int slewThresh = 0; 
-    int ampThresh = 4*255/5;  
+    slewThresh = 0;
+    ampThresh = 3.5*255/5;  
     int dt = 1; //10ms
-    int EGMVals[5];
-    int slewVals[4];
+    int EGMVals[3];
+    int slewVals[2];
     int slewSum = 0;
     int i;
     int slewAvg;
     
     //ADC conversion and array storage
-    for(i=0; i<5; i++){
+    for(i=0; i<3; i++){
         ADCON0bits.GO = 1; 
         while(ADCON0bits.GO==0){}; //wait to finish conversion
         EGMVals[i] = ADRESH; //replace with correct pin
-        
+        ADRESL = 0;
+        //ADCON0bits.GO = 0; //disable ADC
         if(i>0){
             slewVals[i] = (EGMVals[i]-EGMVals[i-1])/dt; //replace with correct pin    
             slewSum += slewVals[i];
         }
-        //slewVal = EGMVals[4]-EGMVals[0]; 
-        __delay_ms(1); 
+        
+        __delay_ms(5); 
     }
-
+    
     //calculate avg slew rate
-    slewAvg = slewSum/4;
-    //slewAvg = EGMVals[4] - EGMVals[0];
+    slewAvg = slewSum/2;
+    //slewAvg = EGMVals[3] - EGMVals[0];
     
     //if amplitude + slew rate passed threshold, stateA = 1  
-    if(EGMVals[4]>ampThresh){   
+    
+    if(EGMVals[2]>ampThresh){           
+        stateA = 1;
         
         if (slewAvg>slewThresh){
             stateA = 1;
@@ -107,11 +106,11 @@ void processA(void){ //ADC and calculation of slew rate/threshold
             //LATBbits.LATB0 = 0;
             stateA = 0;
         }
+    
     } else {
         stateA = 0;
         //LATBbits.LATB0 = 0; 
-    }
-              
+    }              
 }
 
 void output(void){
